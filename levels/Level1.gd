35c1e08739f0ce5eafba63c0d33a73f42666e2e0
Kyle Zhou <2584270+kylemsguy@users.noreply.github.com>
2024-10-01 13:31:53 -0700

    Add bullet

--- levels/Level1.gd
+++ levels/Level1.gd
@@ -8,6 +8,9 @@ var snake_body = load("res://entities/snake/snake_body.tscn")
 const width = 705
 const height = 450
 
+var snake = null
+var snake_body_parts = []
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,6 +18,22 @@ func _ready():
 	food.position.x = 100
 	food.position.y = 100
 	add_child(food)
+	
+	snake = snake_head.instantiate()
+	snake.position.x = 400
+	snake.position.y = 400
+	add_child(snake)
+	
+	var snake_body_part = snake_body.instantiate()
+	snake_body_part.position.x = 200
+	snake_body_part.position.y = 200
+	snake_body_parts.append(snake_body_part)
+	add_child(snake_body_part)
+	
+	#var bullet = load("res://entities/snake/bullet.tscn").instantiate()
+	#bullet.position.x = 200
+	#bullet.position.y = 300
+	#add_child(bullet)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
