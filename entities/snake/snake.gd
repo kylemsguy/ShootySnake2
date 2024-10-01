f83b9e71db96002bbd431975b1025fe49b29683c
Kyle Zhou <2584270+kylemsguy@users.noreply.github.com>
2024-10-01 14:44:54 -0700

    Added snake head and it moves!

--- entities/snake/snake.gd
+++ entities/snake/snake.gd
@@ -1,11 +1,44 @@
 extends Node
 
+var snake_body_template = load("res://entities/snake/snake_body.tscn")
+
+var health = 100
+var new_snake_segments = []
+var snake_segments = []
+var last_moved = 0
+var curr_time = 0
+
+func create_body_segment():
+	new_snake_segments.append(snake_body_template.instantiate())
+	
+func move_snake_head():
+	var angle = $SnakeHead.rotation_degrees
+	$SnakeHead.move_forward()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	create_body_segment()
+	for i in range(10):
+		create_body_segment()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	curr_time += delta
+	if curr_time - last_moved < 0.1:
+		return
+		
+	var prev_position = $SnakeHead.position
+	move_snake_head()
+	if new_snake_segments:
+		var new_snake_segment = new_snake_segments.pop_back()
+		new_snake_segment.position = prev_position
+		$SnakeBodyContainer.add_child(new_snake_segment)
+		snake_segments.append(new_snake_segment)
+	else:
+		# shuffle everything forwards
+		for i in range(len(snake_segments)-1, -1, -1):
+			var curr_position = snake_segments[i].position
+			snake_segments[i].position = prev_position
+			prev_position = curr_position
+		
+	last_moved = curr_time
--- levels/Level1.gd
+++ levels/Level1.gd
@@ -1,15 +1,13 @@
 extends Node2D
 
 var food = load("res://entities/food/food.tscn")
-var snake_head = load("res://entities/snake/snake_head.tscn")
-var snake_body = load("res://entities/snake/snake_body.tscn")
+var snake_template = load("res://entities/snake/snake.tscn")
 
 # upper bounds for width/height
 const width = 705
 const height = 450
 
 var snake = null
-var snake_body_parts = []
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,16 +17,10 @@ func _ready():
 	food.position.y = 100
 	add_child(food)
 	
-	snake = snake_head.instantiate()
+	snake = snake_template.instantiate()
 	snake.position.x = 400
 	snake.position.y = 400
 	add_child(snake)
-	
-	var snake_body_part = snake_body.instantiate()
-	snake_body_part.position.x = 200
-	snake_body_part.position.y = 200
-	snake_body_parts.append(snake_body_part)
-	add_child(snake_body_part)
 	
 	#var bullet = load("res://entities/snake/bullet.tscn").instantiate()
 	#bullet.position.x = 200
